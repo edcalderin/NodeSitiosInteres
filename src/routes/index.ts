bb86c7947abf4ff0ba6118bea9cf6202b01442a2
edcalderin <edcm.erick@gmail.com>
2019-10-13 23:39:55 -0300

    .

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express'
 import SitesControllers from '../controllers/SitesController'
 import { checkSiteBody } from '../validator'
-
+import {oneOf} from 'express-validator'
 const router = Router()
 const sitesControllers = new SitesControllers()
 
 router.route('/')
     .get(sitesControllers.getSites.bind(sitesControllers))
-    .post(checkSiteBody(), sitesControllers.createSite.bind(sitesControllers))
-    .put(checkSiteBody(), sitesControllers.updateSite.bind(sitesControllers))
+    .post(oneOf(checkSiteBody()), sitesControllers.createSite.bind(sitesControllers))
+    .put(oneOf(checkSiteBody()), sitesControllers.updateSite.bind(sitesControllers))
 
 router.route('/:id')
     .get(sitesControllers.getSiteById)
--- src/validator/index.ts
+++ src/validator/index.ts
@@ -3,21 +3,22 @@ import { check } from 'express-validator'
 export function checkSiteBody(): Array<any> {
     return [
         check('id')
-            .exists().withMessage('La propiedad id es requerida')
+            .exists({ checkFalsy: true, checkNull: true }).withMessage('La propiedad id es requerida')
             .isInt({ min: 0 }).withMessage('No se permite valores negativos ni flotantes'),
 
         check('url_imagen')
-            .exists().withMessage('La propiedad url_imagen es requerida')
+            .exists({ checkFalsy: true, checkNull: true }).withMessage('La propiedad url_imagen es requerida')
             .isURL().withMessage('URL incorrecta'),
 
         check('ubicacion._lat')
-            .exists().withMessage('La propiedad ubicacion._lat es requerida')
+            .exists({ checkFalsy: true, checkNull: true }).withMessage('La propiedad ubicacion._lat es requerida')
             .isNumeric().withMessage('Ingrese un valor numerico'),
 
         check('ubicacion._lon')
-            .exists().withMessage('La propiedad ubicacion._lon es requerida')
+            .exists({ checkFalsy: true, checkNull: true }).withMessage('La propiedad ubicacion._lon es requerida')
             .isNumeric().withMessage('Ingrese un valor numerico')
 
     ]
 }
+
 
